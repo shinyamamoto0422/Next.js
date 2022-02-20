7a68e6dd961e709b099e2eab4596506c0aed4f4a
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-02-20 23:08:09 +0900

    first commit

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head"
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <img src="/images/profile.jpeg" alt="First Post" />
+    </>    
+  )
+}
